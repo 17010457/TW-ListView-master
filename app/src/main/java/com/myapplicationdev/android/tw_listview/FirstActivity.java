d8c18bebcbeda86d494a9f1d099c4482b69d6f72
17010489 <elainenkx@gmail.com>
2019-04-30 13:53:26 +0800

    First Activity Commit

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -18,9 +21,35 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv = this.findViewById(R.id.lvYear);
+
 
         al = new ArrayList<String>();
         al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
+
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,al);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition = position;
+                if (itemPosition == 0) {
+                    Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                    intent.putExtra("year", "1");
+                }else if ( itemPosition ==1) {
+                    Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                    intent.putExtra("year", "2");
+                }else {
+                    Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                    intent.putExtra("year", "3");
+                }
+            }
+
+
+        });
 
 
     }
